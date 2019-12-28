c8a7cd76ac4f21021df70575bae31f719189558e
karthik <53786001+karthik1896@users.noreply.github.com>
2019-12-28 08:23:43 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/bootleg_wt88047.mk
+    $(LOCAL_DIR)/colt_wt88047.mk
 
 COMMON_LUNCH_CHOICES := \
-    bootleg_wt88047-user \
-    bootleg_wt88047-userdebug \
-    bootleg_wt88047-eng
+    colt_wt88047-user \
+    colt_wt88047-userdebug \
+    colt_wt88047-eng
